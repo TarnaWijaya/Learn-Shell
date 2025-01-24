9b402a92378c661a7d75d95785eeb9052b2b38bd
Tarna <158784522+TarnaWijaya@users.noreply.github.com>
2025-01-24 23:45:41 +0700

    create MATERIAL 2 [ variable ]/day-1.sh

---
+++ MATERIAL 2 [ variable ]/day-1.sh
@@ -0,0 +1,13 @@
+#!/system/bin/sh
+
+# Belajar Variable
+var="hello world"
+echo "$var" # menginput ( var ) kedalam printah ( echo )
+
+# Variable bisa di berikan nama apa saja dan bebas.
+# Contohnya  ( test="hello world" ) dan di dalam
+# variable bebas mau di isi apa saja.
+
+# NOTE : jika isi variable ada space/jarak Contohnya ( hello world ) maka harus menggunakan tanda kutip dua, jika di dalam variablenya tanpa space misalnya ( HelloWorld ) maka di perbolehkan tanpa menggunakan tanda kutip dua.
+
+# TUJUAN : untuk menghindari resiko error pada program shell yang di jalankan.
