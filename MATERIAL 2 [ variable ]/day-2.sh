0f27fc285eac1280e3904f3bed34956c779d2f5b
Tarna <158784522+TarnaWijaya@users.noreply.github.com>
2025-01-24 23:45:56 +0700

    create MATERIAL 2 [ variable ]/day-2.sh

---
+++ MATERIAL 2 [ variable ]/day-2.sh
@@ -0,0 +1,7 @@
+#!/system/bin/sh
+
+# Variable input runtime
+var=$1
+echo "$var"
+
+# Ketika script di jalankan, contohnya ( sh variable.sh Belajar ) maka output atau pesan yang muncul text ( Belajar ).
