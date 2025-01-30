ae64cb8af45fadb8637af672e242692ad6b56ad0
Tarna <158784522+TarnaWijaya@users.noreply.github.com>
2025-01-30 22:24:53 +0700

    Baru

---
+++ MATERIAL 5 [ looping ]/day-1.sh
@@ -0,0 +1,8 @@
+#!/system/bin/sh
+echo "hello world"
+sleep 5
+sh /sdcard/loop.sh
+
+# ini adalah program looping sederhana
+# NOTE : berikan sleep 3/5 agar system
+# tidak mengalami lag atau bug
---
+++ MATERIAL 5 [ looping ]/day-2.sh
@@ -0,0 +1,12 @@
+#!/system/bin/sh
+
+for i in 1 2 3 4 5; do
+  echo "Angka: $i"
+done
+
+# menghitung angka 1 sampai 5.
+# program ini bersifat perulangan
+# jadi data angka 1 sampai 5
+# akan terus di input dan di ulang
+# jika datanya sudah di ulang sampai
+# 5x maka program akan berhenti.
